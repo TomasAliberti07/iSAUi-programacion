855c98ee863724ca55f301e67bdd685cd42edbd2
Tomas <tomasalibertiquinto.sociales.ma@gmail.com>
2024-05-22 13:43:13 -0300

    "Se modifico ejercicio6"

--- ejercicio6.py
+++ ejercicio6.py
@@ -6,11 +6,15 @@
 #El programa debe informar al usuario si la contraseña es válida o no
 print("Escriba una contraseña que contenga al menos 8 caracteres,una mayuscula,un numero y un caracter especial(#$%&/()=?¡¿):")
 contraseña=input("")
-if  len(contraseña)>=8 :
-    contraseñam=["A-Z"]
-    contraseñamin=["a-z"]
-    contraseñasim=["!#$%&/()=?¡¿"]
-    contraseñanum=[1234567890]
-    print("La contraseña fue creada correctamente :)")
-else:
+while True:
+ if  len(contraseña)>=8 :
+    if any(c.isupper() for c in contraseña):#if any verifica si la contraseña contiene al menos una mayuscula y for que recorra cada palabra o digito de la contraseña
+        if any(c.islower() for c in contraseña):#Verifica si tiene minuscula con la funcion islower y lo recorre por un for 
+            if any(c in ".:;,!#$%&/()=?¡¿" for c in contraseña):#Verifica si contiene uno de los caracteres especiales
+            
+                if any(c.isdigit() for c in contraseña):#Verifica si contiene al menos un numero
+                     print("La contraseña fue creada correctamente :)")
+                     break
+ else:
     print("Hubo un error por favor vuelva a intentarlo...")
+    input("")
