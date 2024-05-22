6ee74289f5bd496cd222da22b2bdd48536aa6f19
Tomas <tomasalibertiquinto.sociales.ma@gmail.com>
2024-05-22 13:44:11 -0300

    "Se agrego el ejercicio1 de lista"

---
+++ ejerlista1.py
@@ -0,0 +1,5 @@
+#Suma de elementos: Escribe una función que tome una lista de números y devuelva la
+#suma de todos los elementos en la lista.
+lista=[10,50,40,50]#Definimos la lista con los valores 
+suma=sum(lista)#Utilizo la funcion sum  suma todos los elementos dentro de la lista
+print(f"De la lista {lista}la suma es:{suma}")#Pido que me imprima la lista y el resultado
