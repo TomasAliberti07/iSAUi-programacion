34d98187fdfcf1adccea7a612f8656eefec50859
Tomas <tomasalibertiquinto.sociales.ma@gmail.com>
2024-05-24 17:42:55 -0300

    "Se modifico ejercicio 0"

--- ejercicio0.py
+++ ejercicio0.py
@@ -1,5 +1,5 @@
 #Escribir un progama en Python que calcule el indice de masa corporal(IMC) de una persona.
-peso=float(input("Ingrese su peso:"))
-altura=float(input("Ingrese su altura:"))
-calculo=peso/(altura**2)
-print("El IMC es de:", calculo)
+peso=float(input("Ingrese su peso:"))#Le pido al usuario que ingrese su peso
+altura=float(input("Ingrese su altura:"))#Le pido al usuario que ingrese su altura
+calculo=peso/(altura**2)#Hago el calculo necesario
+print("El IMC es de:", calculo)#Imrpimo el calculo
