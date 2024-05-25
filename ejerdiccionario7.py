d479fd4016d26cecd573b076c206992c0de3dadb
Tomas <tomasalibertiquinto.sociales.ma@gmail.com>
2024-05-24 21:11:35 -0300

    "Se actualizo ejercicio 7 de diccionario"

--- ejerdiccionario7.py
+++ ejerdiccionario7.py
@@ -1,16 +1,10 @@
-#Diccionario de frecuencias: Escribe una función que reciba una lista de palabras y
-#devuelva un diccionario con la frecuencia de cada palabra.
-palabras=input("Ingrese las palabras separadas por coma:")#Ingresar las palabras
-palabras=palabras.split(",")#Separar por coma cada palabra
-lista=palabras#Las palabras se guardan en una lista
-def frecuencias(lista):#Defino la funcion frecuencias con el argumento lista
-    diccionario={}#Creo un diccionario vacio
-    for palabra in lista:#Va a iterar sobre cada palabra de la lista
-        if palabra in diccionario:#Si la palabra esta en el diccionario 
-            diccionario[palabra]+=1#Aumenta la frecuencia en 1
-        else:
-            diccionario[palabra]=1#Si la palabra no esta en el diccionario la agrega con una frecuencia de 1
-    return diccionario#Devuelve diccionario
-las_frecuencias=frecuencias(palabras)#Guardo la funcion 
-print(las_frecuencias)#Imprimo el diccionario con las frecuencias
-
+#Sumar valores: Escribe una función que reciba un diccionario con valores numéricos y
+#devuelva la suma de todos los valores.
+diccionario={"a":1,"b":2,"c":3,"d":4,"e":5}#Crea un diccionario con su clave valor
+def sumar_valores(diccionario):#Defino la funcion  suma de valores con el argumento diccionario
+    suma=0#Creo la variable suma con el valor de 0
+    for valor in diccionario.values():#El valor va a tomar cada valor de el diccionario
+        suma+=valor#Suma de los valores
+    return suma#Pido que me devuelva la suma
+resultado=sumar_valores(diccionario)#Guardo la funcion 
+print(f"La suma de las valores es:{resultado}")#Imprimo el resultado
