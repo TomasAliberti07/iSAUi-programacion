feb1b6a347b0af9552da3ae57b93731393644233
Tomas <tomasalibertiquinto.sociales.ma@gmail.com>
2024-06-05 15:25:45 -0300

    "listo"

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 import string
 def contraseña(longitud=12):
@@ -9,16 +8,3 @@ def contraseña(longitud=12):
 longitudmin=12        
 crea_contraseña= contraseña(longitudmin)
 print("Su contraseña es:", crea_contraseña)
-=======
-import random
-import string
-def contraseña(longitud=12):
-    caracter= string.ascii_letters + string.digits + string.punctuation
-    contraseña="".join(random.choice(caracter) for i  in range (longitud))
-    return contraseña
-
-longitudmin=12        
-crea_contraseña= contraseña(longitudmin)
-print("Su contraseña es:", crea_contraseña)
->>>>>>> 46ab3dd27299fd70e777ef512e65a271ce61e649
- 
